544ad648846ff280a9b62b69c79dd670988376fe
tdslinden <linden-k@hotmail.com>
2022-07-07 00:45:34 -0700

    Created Todo App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
 import './App.css';
+// importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
-function App() {
-  return (
+    function App() {
+    // state
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState("all");
+    const [filteredTodos, setFilteredTodos] = useState([]);
+
+    // use effect
+    useEffect(() => {
+        getLocalTodos();
+    }, []); // an empty array means the function runs only when it is first rendered
+
+    useEffect(() => {
+        filterHandler();
+        saveLocalTodos();
+    }, [todos, status]); // place any state that will change in the array
+
+    // functions
+    const filterHandler = () => {
+        switch(status) {
+            case "completed":
+              setFilteredTodos(todos.filter((todo) => todo.completed === true))
+              break;
+            case "uncompleted":
+              setFilteredTodos(todos.filter((todo) => todo.completed === false))
+              break;
+            default:
+              setFilteredTodos(todos)
+              break;
+        }
+    }
+
+    // save to local storage
+    const saveLocalTodos = () => {
+        if (todos.length > 0) {
+            localStorage.setItem("todos", JSON.stringify(todos))
+        }
+    }
+
+    const getLocalTodos = () => {
+        if (localStorage.getItem("todos") === null) {
+            localStorage.setItem("todos", JSON.stringify([]));
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem("todos"));
+            setTodos(todoLocal);
+        }
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <h1>Todo List</h1>
+        </header>
+        <Form
+            todos={todos}
+            setTodos={setTodos}
+            inputText={inputText}
+            setInputText={setInputText}
+            setStatus={setStatus}
+        />
+        <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
     </div>
-  );
+    );
 }
 
 export default App;
